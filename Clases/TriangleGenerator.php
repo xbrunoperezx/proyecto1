f3516f984c4b3de6ce72c60ec3d60eef384bdd76
mitnick <brunovega1985@gmail.com>
2024-11-06 08:41:49 +0100

    Modificando la clases TriangleGenerator del trinagulo para configurarlo para php

--- Clases/TriangleGenerator.php
+++ Clases/TriangleGenerator.php
@@ -1,37 +1,31 @@
 <?php
 
-function generateTriangle($altura): int{
+class TriangleGenerator {
+   
+    function generateTriangle( int $altura): string{
 
-    echo "<pre>";
-    $altura=6;
-    
-    for( $i=0; $i<$altura; $i++){
-        for( $j=$i; $j<$altura-1;$j++){
-            echo "&nbsp";
-        }
-            for( $k=0; $k<$i+$i+1;$k++){
-                echo "*";
-            } 
-
-            echo "<br>";
-    }       
-
-
-    echo "</pre>";
-    
-    return $altura;
-
-}
-
-function comprobarAltura($numero){
-    while($numero < 0){
-        echo "altura negativa";
+        $resultado = "<pre>";
+        
+  
+         for ($i = 1; $i <= $altura; $i++) {
+            
+             for ($j = $altura; $j > $i; $j--) {
+                 $resultado .= " ";
+             }
+            
+             for ($k = 1; $k <= (2 * $i - 1); $k++) {
+                 $resultado .= "*";
+             }
+            
+             $resultado .= "<br>";
+         }
+         $resultado .= "</pre>";
+         return $resultado;
 
     }
-    return $numero;
+    
 }
 
-generateTriangle($altura);
-comprobarAltura($numero);
 
+    
 ?>
